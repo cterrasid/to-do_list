58823f0ebb8aa515b087385cbdc6e4ebbcdcbfcb
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-10 00:44:37 +0200

    Implement vue-router to the project

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,39 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo @add-todo="addTodo"/>
-    <Todos v-bind:todos="todos" @del-todo="deleteTodo" />
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
-import AddTodo from "./components/AddTodo";
 
 export default {
-  name: "App",
+  name: "app",
   components: {
-    Todos,
-    Header,
-    AddTodo,
-  },
-  data() {
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo 1",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo 2",
-          completed: true,
-        },
-        {
-          id: 3,
-          title: "Todo 3",
-          completed: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo]
-    }
-  },
-};
+    Header
+  }
+}
 </script>
-
 <style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
 
-body {
-  font-family: "Avenir", Helvetica, sans-serif;
-  line-height: 1.4;
+#nav {
+  padding: 30px;
 }
 
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #fff;
-  padding: 7px 20px;
-  cursor: pointer;
-}
-.btn:hover {
-  background: #666;
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
 }
 
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
