39674d2c9ca1c1dc8932f8ab7f53d16a9c59696e
Clarette Terrasi Díaz <clarette.terrasi@gmail.com>
2020-09-09 21:46:41 +0200

    Created method to add todos

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
     <Header />
-    <Todos v-bind:todos="todos" @del-todo="deleteTodo"/>
+    <AddTodo @add-todo="addTodo"/>
+    <Todos v-bind:todos="todos" @del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos";
 import Header from "./components/layout/Header";
+import AddTodo from "./components/AddTodo";
 
 export default {
   name: "App",
   components: {
     Todos,
-    Header
+    Header,
+    AddTodo,
   },
   data() {
     return {
@@ -37,10 +40,13 @@ export default {
     };
   },
   methods: {
-    deleteTodo(id){
-      this.todos = this.todos.filter(todo => todo.id !== id)
+    deleteTodo(id) {
+      this.todos = this.todos.filter((todo) => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo]
     }
-  }
+  },
 };
 </script>
 
@@ -55,4 +61,17 @@ body {
   font-family: "Avenir", Helvetica, sans-serif;
   line-height: 1.4;
 }
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+.btn:hover {
+  background: #666;
+}
+
 </style>
